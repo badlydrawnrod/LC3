8d254e3573c7da0a9f711eefc6a22be19d636063
Rod <rod@badlydrawngames.com>
2020-09-15 22:41:40 +0100

    Initial commit

---
+++ LC3.h
@@ -0,0 +1,336 @@
+/// A header implementation of an LC3 virtual machine.
+///
+/// Use CRTP to supply the ReadMem, WriteMem and Trap methods in the derived class.
+
+#pragma once
+
+#include <cstdint>
+
+namespace lc3
+{
+    /// \brief The core of an LC3 virtual machine.
+    /// \tparam External an implementation of ReadMem, WriteMem and Trap.
+    template<typename External>
+    class VmCore
+    {
+    protected:
+        // VM state. Note that memory access is implemented externally.
+
+        bool running_{false}; // True if the VM is running.
+        uint16_t reg_[8];     // General registers.
+        uint16_t pc_{0x3000}; // Program counter.
+        uint16_t cond_{0};    // Condition flags.
+
+    public:
+        void Run()
+        {
+            constexpr uint16_t PC_START = 0x3000;
+            pc_ = PC_START;
+
+            running_ = true;
+            while (running_)
+            {
+                const uint16_t instr = ReadMem(pc_++);
+                const uint16_t op = instr >> 12;
+
+                switch (op)
+                {
+                case OP_ADD:
+                    OpAdd(instr);
+                    break;
+
+                case OP_AND:
+                    OpAnd(instr);
+                    break;
+
+                case OP_NOT:
+                    OpNot(instr);
+                    break;
+
+                case OP_BR:
+                    OpBr(instr);
+                    break;
+
+                case OP_JMP:
+                    OpJmp(instr);
+                    break;
+
+                case OP_JSR:
+                    OpJsr(instr);
+                    break;
+
+                case OP_LD:
+                    OpLd(instr);
+                    break;
+
+                case OP_LDI:
+                    OpLdi(instr);
+                    break;
+
+                case OP_LDR:
+                    OpLdr(instr);
+                    break;
+
+                case OP_LEA:
+                    OpLea(instr);
+                    break;
+
+                case OP_ST:
+                    OpSt(instr);
+                    break;
+
+                case OP_STI:
+                    OpSti(instr);
+                    break;
+
+                case OP_STR:
+                    OpStr(instr);
+                    break;
+
+                case OP_TRAP:
+                    OpTrap(instr);
+                    break;
+
+                case OP_RES:
+                case OP_RTI:
+                default:
+                    /* BAD OPCODE */
+                    abort();
+                }
+            }
+        }
+
+    private:
+        enum class Flags
+        {
+            POS = 1 << 0,  // P
+            ZERO = 1 << 1, // Z
+            NEG = 1 << 2,  // N
+        };
+
+        enum Opcodes
+        {
+            OP_BR = 0, // Branch.
+            OP_ADD,    // Add.
+            OP_LD,     // Load.
+            OP_ST,     // Store.
+            OP_JSR,    // Jump to subroutine.
+            OP_AND,    // Bitwise and.
+            OP_LDR,    // Load register.
+            OP_STR,    // Store register.
+            OP_RTI,    // Return from interrupt (not currently implemented).
+            OP_NOT,    // Bitwise not.
+            OP_LDI,    // Load indirect.
+            OP_STI,    // Store indirect.
+            OP_JMP,    // Jump.
+            OP_RES,    // Reserved (unused).
+            OP_LEA,    // Load effective address.
+            OP_TRAP    // Invoke a trap.
+        };
+
+        // Helpers to invoke "underlying" methods.
+
+        /// \brief Casts to the underlying External type.
+        External& AsExternal() { return static_cast<External&>(*this); }
+
+        /// \brief Reads from memory at the given address.
+        /// \param address the address to read from.
+        /// \return the word at the given address.
+        uint16_t ReadMem(uint16_t address)
+        {
+            return AsExternal().ReadMem(address);
+        }
+
+        /// \brief Writes to memory at the given address.
+        /// \param address the address to write to.
+        /// \param val the value to write to the addres.
+        void WriteMem(uint16_t address, uint16_t val)
+        {
+            AsExternal().WriteMem(address, val);
+        }
+
+        /// \brief Executes a trap instruction.
+        /// \param instr the trap to execute (0-255).
+        void Trap(const uint16_t instr)
+        {
+            AsExternal().Trap(instr);
+        }
+
+        // Ancillary methods.
+
+        static uint16_t SignExtend(uint16_t x, int bitCount)
+        {
+            if ((x >> (bitCount - 1)) & 1)
+            {
+                x |= (0xffff << bitCount);
+            }
+            return x;
+        }
+
+        void UpdateFlags(uint16_t r)
+        {
+            if (reg_[r] == 0)
+            {
+                cond_ = static_cast<uint16_t>(Flags::ZERO);
+            }
+            else if (reg_[r] >> 15)
+            {
+                cond_ = static_cast<uint16_t>(Flags::NEG);
+            }
+            else
+            {
+                cond_ = static_cast<uint16_t>(Flags::POS);
+            }
+        }
+
+        // Instruction decoding.
+
+        static bool IsImmediate(const uint16_t instr) { return (instr >> 5) & 0x1; }
+        static bool IsLong(const uint16_t instr) { return (instr >> 11) & 1; }
+        static uint16_t Cond(const uint16_t instr) { return (instr >> 9) & 0x7; }
+        static uint16_t Dr(const uint16_t instr) { return (instr >> 9) & 0x7; }
+        static uint16_t Sr1(const uint16_t instr) { return (instr >> 6) & 0x7; }
+        static uint16_t Sr2(const uint16_t instr) { return instr & 0x7; }
+        static uint16_t Sr(const uint16_t instr) { return (instr >> 9) & 0x7; }
+        static uint16_t BaseR(const uint16_t instr) { return (instr >> 6) & 0x7; }
+        static uint16_t Imm5(const uint16_t instr) { return SignExtend(instr & 0x1F, 5); }
+        static uint16_t Offset6(const uint16_t instr) { return SignExtend(instr & 0x3F, 6); }
+        static uint16_t PcOffset9(const uint16_t instr) { return SignExtend(instr & 0x1FF, 9); }
+        static uint16_t PcOffset11(const uint16_t instr) { return SignExtend(instr & 0x7FF, 11); }
+
+        // Opcodes.
+
+        void OpAdd(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t sr1 = Sr1(instr);
+            if (IsImmediate(instr))
+            {
+                const uint16_t imm5 = Imm5(instr);
+                reg_[dr] = reg_[sr1] + imm5;
+            }
+            else
+            {
+                const uint16_t sr2 = Sr2(instr);
+                reg_[dr] = reg_[sr1] + reg_[sr2];
+            }
+            UpdateFlags(dr);
+        }
+
+        void OpAnd(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t sr1 = Sr1(instr);
+            if (IsImmediate(instr))
+            {
+                const uint16_t imm5 = Imm5(instr);
+                reg_[dr] = reg_[sr1] & imm5;
+            }
+            else
+            {
+                const uint16_t sr2 = Sr2(instr);
+                reg_[dr] = reg_[sr1] & reg_[sr2];
+            }
+            UpdateFlags(dr);
+        }
+
+        void OpBr(const uint16_t instr)
+        {
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            const uint16_t cond = Cond(instr);
+            if (cond == 0 || (cond & cond_))
+            {
+                pc_ += pcOffset9;
+            }
+        }
+
+        void OpJmp(const uint16_t instr)
+        {
+            const uint16_t baser = BaseR(instr);
+            pc_ = reg_[baser];
+        }
+
+        void OpJsr(const uint16_t instr)
+        {
+            reg_[7] = pc_;
+            if (IsLong(instr))
+            {
+                const uint16_t pcOffset11 = PcOffset11(instr);
+                pc_ += pcOffset11; // JSR.
+            }
+            else
+            {
+                const uint16_t baser = BaseR(instr);
+                pc_ = reg_[baser]; // JSRR.
+            }
+        }
+
+        void OpLd(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            reg_[dr] = ReadMem(pc_ + pcOffset9);
+            UpdateFlags(dr);
+        }
+
+        void OpLdi(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            reg_[dr] = ReadMem(ReadMem(pc_ + pcOffset9));
+            UpdateFlags(dr);
+        }
+
+        void OpLdr(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t baser = BaseR(instr);
+            const uint16_t offset6 = Offset6(instr);
+            reg_[dr] = ReadMem(reg_[baser] + offset6);
+            UpdateFlags(dr);
+        }
+
+        void OpLea(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            reg_[dr] = pc_ + pcOffset9;
+            UpdateFlags(dr);
+        }
+
+        void OpNot(const uint16_t instr)
+        {
+            const uint16_t dr = Dr(instr);
+            const uint16_t sr1 = Sr1(instr);
+            reg_[dr] = ~reg_[sr1];
+            UpdateFlags(dr);
+        }
+
+        void OpSt(const uint16_t instr)
+        {
+            const uint16_t sr = Sr(instr);
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            WriteMem(pc_ + pcOffset9, reg_[sr]);
+        }
+
+        void OpSti(const uint16_t instr)
+        {
+            const uint16_t sr = Sr(instr);
+            const uint16_t pcOffset9 = PcOffset9(instr);
+            WriteMem(ReadMem(pc_ + pcOffset9), reg_[sr]);
+        }
+
+        void OpStr(const uint16_t instr)
+        {
+            const uint16_t sr = Sr(instr);
+            const uint16_t baser = BaseR(instr);
+            const uint16_t offset6 = Offset6(instr);
+            WriteMem(reg_[baser] + offset6, reg_[sr]);
+        }
+
+        void OpTrap(const uint16_t instr)
+        {
+            Trap(instr);
+        }
+    };
+} // namespace lc3
---
+++ main.cpp
@@ -0,0 +1,216 @@
+#include "LC3.h"
+
+#include <Windows.h>
+#include <conio.h>
+#include <csignal>
+#include <cstdint>
+#include <cstdio>
+
+namespace
+{
+    HANDLE hStdin = INVALID_HANDLE_VALUE;
+
+    bool CheckKey()
+    {
+        return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+    }
+
+    DWORD oldMode;
+
+    void DisableInputBuffering()
+    {
+        hStdin = GetStdHandle(STD_INPUT_HANDLE);
+
+        // Save the old input mode.
+        GetConsoleMode(hStdin, &oldMode); /* save old mode */
+
+        // Disable echo and line input.
+        DWORD newMode = oldMode ^ ENABLE_ECHO_INPUT ^ ENABLE_LINE_INPUT;
+        SetConsoleMode(hStdin, newMode);
+
+        // Clear the input buffer.
+        FlushConsoleInputBuffer(hStdin);
+    }
+
+    void RestoreInputBuffering()
+    {
+        SetConsoleMode(hStdin, oldMode);
+    }
+
+    void HandleInterrupt(int signal)
+    {
+        RestoreInputBuffering();
+        printf("\n");
+        exit(-2);
+    }
+} // namespace
+
+class Lc3Vm : public lc3::VmCore<Lc3Vm>
+{
+private:
+    enum class Traps
+    {
+        TRAP_GETC = 0x20,  // Get character from keyboard, not echoed onto the terminal.
+        TRAP_OUT = 0x21,   // Output a character.
+        TRAP_PUTS = 0x22,  // Output a word string.
+        TRAP_IN = 0x23,    // Get character from keyboard, echoed onto the terminal.
+        TRAP_PUTSP = 0x24, // Output a byte string.
+        TRAP_HALT = 0x25   // Halt the program.
+    };
+
+    // Memory - 65536 x 16-bit locations (i.e., not bytes).
+    uint16_t mem_[65536];
+
+    static uint16_t Swap16(uint16_t x) { return (x << 8) | (x >> 8); }
+
+public:
+    // CRTP methods to be invoked from the base class.
+    void WriteMem(uint16_t address, uint16_t val) { mem_[address] = val; }
+    uint16_t ReadMem(uint16_t address);
+    void Trap(const uint16_t instr);
+
+public:
+    void ReadImage(FILE* file);
+    bool ReadImage(const char* image_path);
+};
+
+uint16_t Lc3Vm::ReadMem(uint16_t address)
+{
+    // External mapped I/O ports.
+    constexpr uint16_t MR_KBSR = 0xFE00; // Keyboard status register.
+    constexpr uint16_t MR_KBDR = 0xFE02; // Keyboard data register.
+
+    if (address == MR_KBSR)
+    {
+        // The VM is trying to read the keyboard.
+        if (CheckKey())
+        {
+            mem_[MR_KBSR] = (1 << 15);
+            mem_[MR_KBDR] = getchar();
+        }
+        else
+        {
+            mem_[MR_KBSR] = 0;
+        }
+    }
+    return mem_[address];
+}
+
+void Lc3Vm::Trap(const uint16_t instr)
+{
+    switch (static_cast<Traps>(instr & 0xFF))
+    {
+    case Traps::TRAP_GETC:
+        // Trap GETC - read a single character.
+        reg_[0] = static_cast<uint16_t>(getchar());
+        break;
+
+    case Traps::TRAP_OUT:
+        // Trap OUT - write a single character.
+        putc((char)reg_[0], stdout);
+        fflush(stdout);
+        break;
+
+    case Traps::TRAP_PUTS:
+        // Trap PUTS - write a character string.
+        {
+            /* one char per word */
+            uint16_t* c = mem_ + reg_[0];
+            while (*c)
+            {
+                putc((char)*c, stdout);
+                ++c;
+            }
+            fflush(stdout);
+        }
+        break;
+
+    case Traps::TRAP_IN:
+        // Trap IN - read a single character.
+        {
+            printf("Enter a character: ");
+            char c = getchar();
+            putc(c, stdout);
+            reg_[0] = static_cast<uint16_t>(c);
+        }
+        break;
+
+    case Traps::TRAP_PUTSP:
+        // Trap PUTSP - write a big-endian byte-packed character string.
+        {
+            uint16_t* c = mem_ + reg_[0];
+            while (*c)
+            {
+                char char1 = (*c) & 0xFF;
+                putc(char1, stdout);
+                char char2 = (*c) >> 8;
+                if (char2) putc(char2, stdout);
+                ++c;
+            }
+            fflush(stdout);
+        }
+        break;
+
+    case Traps::TRAP_HALT:
+        // Trap HALT - and catch fire.
+        puts("HALT");
+        fflush(stdout);
+        running_ = false;
+        break;
+    }
+}
+
+void Lc3Vm::ReadImage(FILE* file)
+{
+    // The origin tells us where in memory to place the image.
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = Swap16(origin);
+
+    // We know the maximum file size so we only need one fread.
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = mem_ + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    // Swap to little endian.
+    while (read-- > 0)
+    {
+        *p = Swap16(*p);
+        ++p;
+    }
+}
+
+bool Lc3Vm::ReadImage(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return false; }
+    ReadImage(file);
+    fclose(file);
+    return true;
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    Lc3Vm lc3;
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!lc3.ReadImage(argv[i]))
+        {
+            printf("failed to load image: %s\n", argv[i]);
+            exit(1);
+        }
+    }
+
+    signal(SIGINT, HandleInterrupt);
+    DisableInputBuffering();
+
+    lc3.Run();
+
+    RestoreInputBuffering();
+}
